c8dda5ad23e25e88144bf9f0581d8c835c0b760f
Song-Eo <djwlsthd@naver.com>
2023-06-01 09:12:02 +0900

    update 2023.06.01

--- aduino/Serial/Serial.ino
+++ aduino/Serial/Serial.ino
@@ -7,16 +7,22 @@
 #define RST_PIN 9
 
 MFRC522 rfid(SS_PIN, RST_PIN);
-MFRC522::MIFARE_Key key; 
 
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 String printing = "------GasLevel------";
 String gas_level;
 
+bool flag1, flag2, flag3;
+int rf;
+
 byte nuidPICC[4];
 
 void setup()
 {
+  rf = 0;
+  flag1 = false;
+  flag2 = false;
+  flag3 = false;
   Serial.begin(9600);
   lcd.begin();
   lcd.backlight();
@@ -28,28 +34,74 @@ void setup()
   SPI.begin(); // Init SPI bus
   rfid.PCD_Init(); // Init MFRC522 
 
-    for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
-  }
-  pinMode(3, OUTPUT);
 }
 
 void loop()
 {
+  delay(500);
   if (Serial.available() > 0){
-    gas_level = "0";
     lcd.setCursor(9, 2);
     char s = Serial.read();
-    gas_level.concat(s);
+    if(s=='1')
+      gas_level = "01";
+    else if(s=='2')
+      gas_level = "02";
+    else if(s=='3')
+      gas_level = "03";
+    else if(s=='4')
+      gas_level = "04";
+    else if(s=='5')
+      gas_level = "05";
     lcd.print(gas_level);
   }
+  if(analogRead(0)<500 && !flag1){
+    Serial.print("rain");
+    flag1 = true;
+  }
+
+  if(analogRead(1)<500 && !flag2){
+    Serial.print("mois");
+    flag2 = true;
+  }
+
+  if(digitalRead(7) && !flag3){
+    Serial.print("undetect");
+    flag3 = true;
+  }
+  
+  // 카드가 인식되었다면 다음으로 넘어가고 아니면 더이상 
+  // 실행 안하고 리턴
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
 
+  // ID가 읽혀졌다면 다음으로 넘어가고 아니면 더이상 
+  // 실행 안하고 리턴
   if ( ! rfid.PICC_ReadCardSerial())
     return;
 
-  digitalWrite(3, HIGH);
-  delay(2000);
-  digitalWrite(3, LOW);
+   if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+   rfid.uid.uidByte[1] != nuidPICC[1] || 
+   rfid.uid.uidByte[2] != nuidPICC[2] || 
+   rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+    if(rf == 0){
+      Serial.print("rfid_first");
+      rf++;
+  }
+    else if (rf == 1){
+      Serial.print("rfid_second");
+     rf++;
+  }
+  }
+
+
+
+    // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
 }
--- aduino/secondUNO/secondUNO.ino
+++ aduino/secondUNO/secondUNO.ino
@@ -1,6 +1,7 @@
-#define RAINDROPMODULE 0
-
 void setup() {
+  flag1 = false;
+  flag2 = false;
+  flag3 = false;
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
@@ -9,14 +10,21 @@ void loop() {
   // put your main code here, to run repeatedly:
   
 
-  if(analogRead(0)<500)
-    Serial.println("Rainy");
+  if(analogRead(0)<500 && !flag1){
+    Serial.print("rain");
+    flag1 = true;
+  }
 
-  if(analogRead(1)<500)
-    Serial.println("Moisture");
+  if(analogRead(1)<500 && !flag2){
+    Serial.print("mois");
+    flag2 = true;
+  }
 
-  if(!digitalRead(7))
-    Serial.println("Detected");
+  if(digitalRead(7) && !flag3){
+    Serial.print("undetect");
+    flag3 = true;
+  }
+    
   delay(750);
 
 }
